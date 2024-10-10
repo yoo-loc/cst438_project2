193f1f61af10f04405118102da11f1dc56339e9a
Tan2731 <Mitan@csumb.edu>
2024-10-10 01:42:08 -0700

    homepage nav

--- src/home/Homepage.js
+++ src/home/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Homepage.css'; // Assuming you'll add some styles here
+import GiftIdeas from '../components/GiftIdeas';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,12 +10,26 @@ const HomePage = () => {
     localStorage.removeItem('userToken');
     navigate('/login');
   };
+  const wishlistButton = () => {
+    navigate('/WishlistHome');
+  };
+  const giftButton = () => {
+    navigate('/GiftIdeas');
+  };
+  
 
   return (
     <div className="homePage">
       <h1>Welcome to your Wishlist</h1>
       <p>This is your homepage after logging in.</p>
-      
+      <br></br>
+
+      <button onClick={wishlistButton} className="btn btn-primary logout-btn">
+        Wishlist
+      </button>
+      <button onClick={giftButton} className="btn btn-primary logout-btn">
+        Gifts
+      </button>
       <button onClick={handleLogout} className="btn btn-primary logout-btn">
         Logout
       </button>
